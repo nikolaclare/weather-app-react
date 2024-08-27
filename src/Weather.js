a2a4c72c74750c2444f0d43cbfccf93fe5bbff48
nikola ashton <132887065+nikolaclare@users.noreply.github.com>
2024-08-28 08:42:26 +1000

    Set background image, added logo and footer

--- src/Weather.js
+++ src/Weather.js
@@ -8,6 +8,13 @@ export default function Weather() {
         <input type="search" placeholder="Enter a city..." />
         <input type="submit" value="Search"/>
       </form>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://nikola-ashton-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer">
+          Nikola Ashton</a>{" "}and is open-sourced on{" "}
+        <a href="https://github.com/nikolaclare/weather-app-react" target="_blank" rel="noopener noreferrer">GitHub</a>{" "}
+        and hosted on {" "}<a href="https://project-weatherapp-react.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>
+      </footer>
     </div>
   )
 }
