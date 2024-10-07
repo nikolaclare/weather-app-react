dfe19a55d98339e0238f408b0f7efade6be13c3e
nikola ashton <132887065+nikolaclare@users.noreply.github.com>
2024-10-08 10:50:56 +1100

    Added new API, added additional weather information, fixed errors

--- src/Weather.js
+++ src/Weather.js
@@ -9,34 +9,45 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
-      temperature: response.data.current.temp,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      feels_like: response.data.temperature.feels_like,
+      wind: response.data.wind.speed,
+      description: response.data.condition.description,
+      icon: response.data.condition.icon,
       city: response.data.city,
     });
   }
-  
+
   function handleSubmit(event) {
     event.preventDefault();
-    const apiKey = "1a738c57f3cdc4d4f4c077e40a2facee";
-    let apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
   }
 
   function handleCityChange(event) {
     setCity(event.target.value);
   }
+  
+  function search() {
+    const apiKey = "04aft6704ff52a4b6b603b4986c4oc2a";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-  return (
+  if (weatherData.ready) {
+    return (
       <div className="Weather">
         <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
-            <input type="search" placeholder="Enter a city..." className="form-control ssearch-input" onChange={handleCityChange} />
+              <input type="search" placeholder="Enter a city..." className="form-control search-input" onChange={handleCityChange} />
             </div>
             <div className="col-3 p-0">
-            <input type="submit" value="Search" className="btn btn-primary w-100" />
-          </div>
+              <input type="submit" value="Search" className="btn btn-primary w-100" />
+            </div>
           </div>
         </form>
+        <WeatherInfo data={weatherData} />
         <footer>
           This project was coded by{" "}
           <a href="https://nikola-ashton-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer">
@@ -45,5 +56,9 @@ export default function Weather(props) {
           and hosted on {" "}<a href="https://project-weatherapp-react.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>
         </footer>
       </div>
-    )
+    );
+  } else {
+    search();
+    return "Loading...";
   }
+}
