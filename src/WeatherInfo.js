10966f9587c605c6e62c23c371c58d55f0b15259
nikola ashton <132887065+nikolaclare@users.noreply.github.com>
2024-10-08 11:15:46 +1100

    complete weatherInfo.js to display default city information on loading

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -4,8 +4,29 @@ export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <div className="row">
-        <div className=
+        <div className="col-6">
+          <h1>{props.data.city}</h1>
+          <ul>
+            <li>
+              {props.data.description}, Feels like: <strong>{Math.round(props.data.feels_like)}°C</strong> 
+            </li>
+            <li>
+              Humidity: <strong>{props.data.humidity}%</strong>, Wind: <strong>{props.data.wind}km/h</strong>
+            </li>
+          </ul>
+        </div>
+        <div className="col-lg-6 col-md-6 col-sm-8">
+          <div className="temperature-container d-flex justify-content-end">
+            <WeatherIcon code={props.data.icon} size={52} />
+            <div>
+              <span className="temperature">
+                {Math.round(props.data.temperature)}
+              </span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
       </div>
-      </div>
+    </div>
   );
 }
